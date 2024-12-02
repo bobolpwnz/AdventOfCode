d0bcd91240ae84681bd6b36eee5f0f9f8859b018
Konrad Łaz <laz.konrad@gmail.com>
2024-12-02 16:00:58 +0100

    day1

--- 2024/source/day01.cpp
+++ 2024/source/day01.cpp
@@ -1,6 +1,7 @@
 #include "days.hpp"
 
 #include "bblp/aoc/file_utils.hpp"
+#include "bblp/aoc/string_utils.hpp"
 
 #include <algorithm>
 #include <cstdlib>
@@ -22,11 +23,49 @@ auto parse(const std::filesystem::path& filePath) {
 }
 
 int calculatePartOne(const std::vector<std::string>& input) {
-    return 0;
+    std::vector<int32_t> leftList, rightList;
+    leftList.reserve(input.size());
+    rightList.reserve(input.size());
+
+    for (auto const& line : input) {
+        const auto parts = split(line, " ");
+        leftList.emplace_back(std::stoul(parts.front()));
+        rightList.emplace_back(std::stoul(parts.back()));
+    }
+    
+    std::sort(leftList.begin(), leftList.end());
+    std::sort(rightList.begin(), rightList.end());
+
+    uint32_t distance = 0U;
+
+    for (auto i = 0U; i < leftList.size(); ++i) {
+        distance += std::abs(leftList.at(i) - rightList.at(i));
+    }
+
+    return distance;
 }
 
 int calculatePartTwo(const std::vector<std::string>& input) {
-    return 0;
+    std::vector<int32_t> leftList, rightList;
+    leftList.reserve(input.size());
+    rightList.reserve(input.size());
+
+    for (auto const& line : input) {
+        const auto parts = split(line, " ");
+        leftList.emplace_back(std::stoul(parts.front()));
+        rightList.emplace_back(std::stoul(parts.back()));
+    }
+
+    std::unordered_map<int32_t, uint32_t> scores;
+    for (auto left : leftList) {
+        auto const count = std::count(rightList.cbegin(), rightList.cend(), left);;
+        scores[left] += left * count;
+    }
+
+    return std::accumulate(scores.cbegin(), scores.cend(), 0U, [](const uint32_t sum, const auto &item) { 
+        return sum + item.second;
+    });
+
 }
 }  // namespace
 
--- 2024/test/test_day01.cpp
+++ 2024/test/test_day01.cpp
@@ -5,7 +5,7 @@
 namespace bblp::aoc::test {
 TEST(Day1, test) {
     const auto result = day01();
-    EXPECT_EQ("", result.first);
-    EXPECT_EQ("", result.second);
+    EXPECT_EQ("1941353", result.first);
+    EXPECT_EQ("22539317", result.second);
 }
 };  // namespace bblp::aoc::test
